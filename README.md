5b185dccedfbd63fcc3bc0b2e71678ee30a2d070
Tim Conkling <tconkling@gmail.com>
2020-01-27 11:24:36 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# PyTorch examples in Streamlit
+
+* [Examples source](https://github.com/pytorch/examples)
+* To run: `streamlit run streamlit_enhance.py`
+
+
+## Wishlist
+
+* `st.slider` takes up way too much space
+* `st.input`: want to restrict to numeric inputs, use numeric input values
+* `st.slider` returns an Array even when we just have a single value
+* `st.write`: support for namedtuple
+* `st.slider`: always giving float values?
+* `st.clear()` as synonym for st.empty (for hiding elements)
+* Sometimes I really don't want to rerun immediately after dragging a slider; instead I want to enter a bunch of data and hit GO
+    - `st.atomic_widget_group()` or something
+    - Something similar to `argparse`
+* `st.image_picker` widget (URL, file, etc)
+* Copy code icon appears in top-right corner of report when using `st.code` in an `st.cache` function
+* I continue to really want to introspect the cache
+* When browser reconnects to new server, input values don't get properly updated
+* My script is hanging after several minutes and I don't know why, or how to debug it (easily)
+* Scriptrunner crash:
+    - (I thought I had fixed this one with `_set_execing_flag`, but apparently not!)
+    ```
+    Exception in thread ScriptRunner.loop:
+Traceback (most recent call last):
+  File "/usr/local/Cellar/python/3.7.3/Frameworks/Python.framework/Versions/3.7/lib/python3.7/threading.py", line 917, in _bootstrap_inner
+    self.run()
+  File "/usr/local/Cellar/python/3.7.3/Frameworks/Python.framework/Versions/3.7/lib/python3.7/threading.py", line 865, in run
+    self._target(*self._args, **self._kwargs)
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/streamlit/ScriptRunner.py", line 328, in _loop
+    self._run_script(event_data)
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/streamlit/ScriptRunner.py", line 483, in _run_script
+    self._run_script(RerunData(argv=None, widget_state=None))
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/streamlit/ScriptRunner.py", line 375, in _run_script
+    self._set_state(ScriptState.RUNNING)
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/streamlit/ScriptRunner.py", line 228, in _set_state
+    self.on_state_changed.send(self._state)
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/blinker/base.py", line 267, in send
+    for receiver in self.receivers_for(sender)]
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/blinker/base.py", line 267, in <listcomp>
+    for receiver in self.receivers_for(sender)]
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/streamlit/ReportContext.py", line 185, in _enqueue_script_state_changed_message
+    self._enqueue_new_report_message()
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/streamlit/ReportContext.py", line 259, in _enqueue_new_report_message
+    self.enqueue(msg)
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/streamlit/ReportContext.py", line 147, in enqueue
+    self._scriptrunner.maybe_handle_execution_control_request()
+  File "/export/streamlit_hackathon_pytorch/venv/lib/python3.7/site-packages/streamlit/ScriptRunner.py", line 297, in maybe_handle_execution_control_request
+    raise RerunException()
+streamlit.ScriptRunner.RerunException
+    ```
+* Multiprocessing results in weirdness ("Hogwild" neural net)
+
+
+## Thoughts
+
+* *Running* the training in streamlit was kinda a pain. *Using* the trained model was much smoother
---
+++ streamlit_enhance.py
@@ -0,0 +1,192 @@
+from collections import namedtuple
+from io import BytesIO
+from math import log10
+from typing import Any
+from urllib.parse import urlparse
+
+import numpy as np
+import requests
+import streamlit as st
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import PIL.Image
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+
+from super_resolution.data import get_training_set, get_test_set
+from super_resolution.model import Net
+
+Opt = namedtuple('Opt', [
+    'upscale_factor',
+    'batchSize',
+    'testBatchSize',
+    'nEpochs',
+    'lr',
+    'cuda',
+    'threads',
+    'seed',
+])
+
+
+@st.cache
+def open_image(src: Any) -> PIL.Image.Image:
+    if isinstance(src, PIL.Image.Image):
+        return src
+
+    if isinstance(src, str):
+        try:
+            p = urlparse(src)
+            if p.scheme:
+                response = requests.get(src)
+                return PIL.Image.open(BytesIO(response.content))
+        except UnicodeDecodeError:
+            pass
+
+        # If not, see if it's a file.
+        try:
+            return PIL.Image.open(src)
+        except:
+            pass
+
+    raise RuntimeError('Unrecognized image: %s' % src)
+
+
+@st.cache(persist=True, suppress_st_warning=True)
+def train(opt: Opt) -> Net:
+    if opt.cuda and not torch.cuda.is_available():
+        raise Exception("No GPU found, please run without --cuda")
+
+    torch.manual_seed(opt.seed)
+
+    device = torch.device("cuda" if opt.cuda else "cpu")
+
+    print('===> Loading datasets')
+    train_set = get_training_set(opt.upscale_factor)
+    test_set = get_test_set(opt.upscale_factor)
+    training_data_loader = DataLoader(
+        dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
+    testing_data_loader = DataLoader(
+        dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
+
+    print('===> Building model')
+    model = Net(upscale_factor=opt.upscale_factor).to(device)
+    criterion = nn.MSELoss()
+
+    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+
+    training_status = st.empty()
+    test_status = st.empty()
+
+    def train(epoch: int):
+        training_status.code('Training... (epoch=%s)' % epoch)
+        epoch_loss = 0
+        for iteration, batch in enumerate(training_data_loader, 1):
+            input, target = batch[0].to(device), batch[1].to(device)
+
+            optimizer.zero_grad()
+            loss = criterion(model(input), target)
+            epoch_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+            training_status.code("===> Epoch[{}/{}]({}/{}): Loss: {:.4f}"
+                                 .format(epoch, opt.nEpochs, iteration, len(training_data_loader), loss.item()))
+
+        training_status.code("===> Epoch {} Complete: Avg. Loss: {:.4f}"
+                             .format(epoch, epoch_loss / len(training_data_loader)))
+
+    def test():
+        avg_psnr = 0
+        with torch.no_grad():
+            for batch in testing_data_loader:
+                input, target = batch[0].to(device), batch[1].to(device)
+
+                prediction = model(input)
+                mse = criterion(prediction, target)
+                psnr = 10 * log10(1 / mse.item())
+                avg_psnr += psnr
+        test_status.code("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+
+    for epoch in range(1, opt.nEpochs + 1):
+        train(epoch)
+        # test()
+
+    return model
+
+
+@st.cache
+def resize_naive(input_image: Any, scale_factor: int) -> PIL.Image.Image:
+    img = open_image(input_image)
+    return img.resize((img.size[0] * scale_factor, img.size[1] * scale_factor), PIL.Image.ANTIALIAS)
+
+
+@st.cache
+def super_resolve(model: Net, input_image: Any, cuda: bool) -> PIL.Image.Image:
+    img = open_image(input_image).convert('YCbCr')
+    y, cb, cr = img.split()
+
+    img_to_tensor = ToTensor()
+    input = img_to_tensor(y).view(1, -1, y.size[1], y.size[0])
+
+    if cuda:
+        model = model.cuda()
+        input = input.cuda()
+
+    out = model(input)
+    out = out.cpu()
+    out_img_y = out[0].detach().numpy()
+    out_img_y *= 255.0
+    out_img_y = out_img_y.clip(0, 255)
+    out_img_y = PIL.Image.fromarray(np.uint8(out_img_y[0]), mode='L')
+
+    out_img_cb = cb.resize(out_img_y.size, PIL.Image.BICUBIC)
+    out_img_cr = cr.resize(out_img_y.size, PIL.Image.BICUBIC)
+    out_img = PIL.Image.merge('YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
+
+    return out_img
+
+
+"""
+# Streamlit: ENHANCE
+
+Pytorch [Super Resolution Example](https://github.com/pytorch/examples/tree/master/super_resolution) 
+in Streamlit
+"""
+
+"""
+## TRAINING PARAMS
+"""
+
+opt = Opt(
+    cuda=st.checkbox('Use CUDA', value=False),
+    upscale_factor=st.slider('Upscale Factor', value=4, min_value=1, max_value=5),
+    batchSize=st.slider('Training Batch Size', value=4, min_value=1, max_value=256),
+    testBatchSize=st.slider('Testing Batch Size', value=100, min_value=1, max_value=256),
+    nEpochs=st.slider('Training Epochs', value=35, min_value=1, max_value=100),
+    lr=0.001,
+    threads=st.slider('Dataloader Threads', value=4, min_value=1, max_value=16),
+    seed=123,
+)
+
+model = train(opt)
+
+
+"""
+## ENHANCE
+"""
+
+# Lena: https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/220px-Lenna_%28test_image%29.png
+# Car: dataset/BSDS300/images/test/21077.jpg
+# Corn: dataset/BSDS300/images/test/58060.jpg
+
+input_image = st.input('Input Image', 'super_resolution/dataset/BSDS300/images/test/16077.jpg')
+
+st.write('Input')
+st.image(open_image(input_image))
+
+st.write('Output')
+st.image(super_resolve(model, input_image, False))
+
+st.write('Naive')
+st.image(resize_naive(input_image, opt.upscale_factor))
